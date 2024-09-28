0861c56a4d8d134c656239e964f52c4dd7b04f65
Jeferson Fernandes <jfernandes.dev@gmail.com>
2024-09-28 10:43:18 -0300

    FEAT: image slider component

---
+++ mobile/src/components/ImageSlider.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Dimensions } from 'react-native';
+import { ScrollView, Box, HStack, Image, Text, Center } from '@gluestack-ui/themed';
+
+interface ImageSliderProps {
+  images: string[];
+  isActive?: boolean;
+}
+
+const { width } = Dimensions.get('window');
+
+export default function ImageSlider({ images, isActive = true}: ImageSliderProps) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleScroll = (event: any) => {
+    const slideSize = event.nativeEvent.layoutMeasurement.width;
+    const index = event.nativeEvent.contentOffset.x / slideSize;
+    const roundIndex = Math.round(index);
+    setActiveIndex(roundIndex);
+  };
+
+  return (
+    <Box h={280} w="$full">
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
+      >
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            alt="Imagem Produto"
+            source={{ uri: image }}
+            resizeMode="cover"
+            h={280}
+            style={{ width }}
+          />
+        ))}
+      </ScrollView>
+      <HStack position="absolute" bottom="$2" w="$full" justifyContent="space-between" px="$2">
+        {images.map((_, index) => (
+          <Box
+            key={index}
+            h={3}
+            flex={1}
+            rounded="$md"
+            bg={index === activeIndex ? "$white" : "$gray100"}
+            opacity={index === activeIndex ? 1 : 0.50}
+            marginHorizontal={2}
+          />
+        ))}
+      </HStack>
+
+      {!isActive && (
+        <Center
+          position="absolute"
+          w="$full"
+          h="$full"
+          backgroundColor="rgba(26, 24, 27, 0.5)"
+        >
+          <Text color="$gray100" fontSize="$sm" fontFamily="$heading" >
+            ANÚNCIO DESATIVADO
+          </Text>
+        </Center>
+      )}
+    </Box>
+  );
+}
--- mobile/src/screens/AdDetails.tsx
+++ mobile/src/screens/AdDetails.tsx
@@ -1,13 +1,24 @@
-import { Center, Text, VStack } from "@gluestack-ui/themed";
+import { VStack } from "@gluestack-ui/themed";
+import { Power, Trash } from "lucide-react-native";
 
 import { Button } from "@components/Button";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Power, Trash } from "lucide-react-native";
+import ImageSlider from "@components/ImageSlider";
 
 export function AdDetails() {
   return (
     <VStack flex={1} justifyContent="space-between" pb="$6">
       <ScreenHeader showBackButton showEditButton />
+
+      <VStack flex={1} mt="$4">
+        <ImageSlider images={
+          [
+            "https://images.unsplash.com/photo-1460353581641-37baddab0fa2",
+            "https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717",
+            "https://images.unsplash.com/photo-1529810313688-44ea1c2d81d3"
+          ]
+        }/>
+      </VStack>
 
       <VStack px="$8">
         <Button 
